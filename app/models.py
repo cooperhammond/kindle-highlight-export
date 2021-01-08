f49af12ceadd029fd2c1975e518245440f0f646f
Cooper Hammond <kepoorh@gmail.com>
2021-01-07 17:24:36 -0700

    v2 of models with relational columns

--- app/models.py
+++ app/models.py
@@ -3,14 +3,15 @@ from . import db
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-    name = db.Column(db.String(100))
-    gmail_email = db.Column(db.String(100))
-    gmail_password = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    gmail_email = db.Column(db.String(100), nullable=True)
+    gmail_password = db.Column(db.String(100), nullable=True)
+    exports = db.relationship('Export', backref='user', lazy=True)
 
 class Export(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer)
-    title = db.Column(db.String(200))
-    text = db.Column(db.Text)
+    title = db.Column(db.String(200), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
