83dee1e2dc04acc1fd1c18dce06f3dcfd9c2065c
Cooper Hammond <kepoorh@gmail.com>
2021-01-24 11:39:35 -0700

    MVP

--- app/exportslib/__init__.py
+++ app/exportslib/__init__.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 from .gmail_dl import retrieve_kindle_highlight_files
 from .kindle_export_scraper import format_highlight_export, get_parsed_highlight_export
@@ -6,14 +7,17 @@ from .kindle_export_scraper import format_highlight_export, get_parsed_highlight
 dl_directory = "notebook-downloads/"
 
 
-def get_formatted_exports(gmail_username, gmail_password):
+def get_formatted_exports(gmail_username, gmail_password, clearTemp=True):
     exports = {} # book title -> formatted export
 
     file_locs = retrieve_kindle_highlight_files(gmail_username, gmail_password, dl_directory)
 
     for file_loc in file_locs:
-        title = os.path.splitext(os.path.basename(file_loc))[0]
+        title = os.path.splitext(os.path.basename(file_loc))[0].split(" - Notebook")[0]
         raw_exports = get_parsed_highlight_export(file_loc)
         exports[title] = format_highlight_export(*raw_exports)
+
+    if clearTemp:
+        shutil.rmtree(dl_directory)
 
     return exports
--- app/main.py
+++ app/main.py
@@ -5,8 +5,9 @@ from flask.helpers import url_for
 from flask_login import login_required, current_user
 from . import db
 
-from .models import User
+from .models import User, Export
 from .exportslib.gmail_dl import test_gmail_login
+from .exportslib import get_formatted_exports
 
 
 main = Blueprint('main', __name__)
@@ -14,11 +15,7 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    if not current_user.is_authenticated:
-        return redirect(url_for('auth.login'))
-
     return render_template('index.html')
-
 
 @main.route('/about')
 def about():
@@ -46,10 +43,53 @@ def profile_post():
     
     user = User.query.filter_by(id=current_user.id).first()
     user.gmail_email = gmail_email
-    # NOTE: this password is not hashed, it is stored plaintext. This is because the plaintext password is needed
-    # to interface with gmail's IMAP. DO NOT USE A PASSWORD THAT SECURES ANY OF YOUR OTHER ONLINE ACCOUNTS.
+    # NOTE: this password is not hashed, it is stored plaintext. This is because the hashed password would be
+    # useless to interface with gmail's IMAP. DO NOT USE A PASSWORD THAT SECURES ANY OF YOUR OTHER ONLINE ACCOUNTS.
     user.gmail_password = gmail_password
 
     db.session.commit()
 
     return redirect(url_for('main.index'))
+
+@main.route('/highlights', methods=['POST'])
+def highlights():
+    gmail_email = request.form.get('email')
+    gmail_password = request.form.get('password')
+
+    if not test_gmail_login(gmail_email, gmail_password):
+        flash("Please check your login details and try again.")
+        return redirect(url_for('main.index'))
+
+    exports = get_formatted_exports(gmail_email, gmail_password)
+
+    # only logged in users should update the database
+    if current_user.is_authenticated:
+        for export_key in exports:
+
+            # check if the export already exists in the database by comparing title and corresponding user 
+            # and if does, update the contents of the export with the newly retrieved export
+            export_already_existed = False
+            existing_exports = Export.query.filter_by(title=export_key)
+            for existing_export in existing_exports:
+                if existing_export.title == export_key and existing_export.user_id == current_user.id:
+                    existing_export.text = exports[export_key]
+                    export_already_existed = True
+                    db.session.add(existing_export)
+        
+            # if the export does not already exist, create and link it to the current_user
+            if not export_already_existed:
+                user = User.query.filter_by(id=current_user.id).first()
+                new_export = Export(title=export_key, text=exports[export_key])
+                user.exports.append(new_export)
+                db.session.add(new_export)
+
+        db.session.commit()
+
+    return render_template('highlights.html', exports=exports)  
+
+@main.route('/highlights', methods=['GET'])
+def highlights_get():
+    if not current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+
+    return render_template('highlights.html', )
